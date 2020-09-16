f04e823430ffa780355e1375dcad6ab95d8361e4
GriffinMB <byatt.griffin@gmail.com>
2020-09-15 22:09:45 -0500

    Add basic list functionality

---
+++ list.rkt
@@ -0,0 +1,19 @@
+#lang lambda-calculus/base
+
+(require "core.rkt" "cond.rkt" "numbers.rkt")
+
+(module+ test
+  (require (only-in racket #%datum) rackunit))
+
+(def nil (λ _ (λ _ true)))
+(def cons (λ h (λ t (λ s ((s (λ s2 ((s2 h) false))) t)))))
+(def head (λ list ((list select-first) select-first)))
+(def tail (λ list (list select-second)))
+(def is-empty? (λ list ((list select-first) select-second)))
+(def length (λ list (((if (is-empty? list)) zero) (succ (length (tail list))))))
+
+(module+ test
+  (check-equal? true (! (is-empty? nil)))
+  (check-equal? a (! (head ((cons a) nil))))
+  (check-equal? 0 (! (lambda->number (length (tail ((cons a) nil))))))
+  (check-equal? 2 (! (lambda->number (length [[cons b] [[cons a] nil]])))))
