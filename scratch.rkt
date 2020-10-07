d73ddb2619681c9d5e042d774066039e7b0435ca
GriffinMB <byatt.griffin@gmail.com>
2020-10-06 20:05:01 -0500

    Minimally functional simple types

--- scratch.rkt
+++ scratch.rkt
@@ -1,27 +1,25 @@
 #lang glc/simply-typed
 
-(: if (Boolean n n))
+(: if (Boolean n n n))
+(: succ (Number Number))
+(: add (Number Number Number))
+(: zero Number)
+(: one Number)
+(: two Number)
+(: three Number)
+(: four Number)
+(: true Boolean)
+(: false Boolean)
+(: lambda->number (Number Void))
 
-(if true hello bye)
+(: a (Number Number Number))
+(def a (λ x (λ y (add (succ one) y))))
 
-((if false) zz zzz)
+(: b (Boolean Boolean))
+(def b (λ x (if x (if x true false) false)))
 
-(lambda->number ((add one) two))
+(: add-x (Number (Number Number)))
+(def add-x (λ x (add x)))
 
-;(: test (-> Any Any))
-;(def test (λ x x))
-;(test hello-world)
-
-(: b (Boolean Any))
-(def b (λ x (if x hello byebye)))
-
-(: a Boolean)
-(def a (λ x (λ y x)))
-
-(b true)
-
-(lambda->number ((λ x (x two two)) add))
-
-
-(: jj Number)
-(def jj (((if false) one) zero))
+(lambda->number (add one two))
+(lambda->number ((add-x four) one))
--- simply-typed/expander.rkt
+++ simply-typed/expander.rkt
@@ -1,114 +1,90 @@
 #lang racket
 
-(require glc/base/expander
+(require (except-in glc/base/expander #%app)
+
          "../shared/cond.rkt"
          "../shared/core.rkt"
          "../shared/numbers.rkt"
          "../shared/list.rkt"
 
-         (rename-in glc/base/expander [#%app glc-app])
-
          (for-syntax racket/syntax syntax/parse racket/list))
 
-(provide (except-out (all-from-out glc/base/expander)
-                     #%module-begin
-                     #%app
-                     def)
+(provide (except-out (all-from-out glc/base/expander) def)
 
          (all-from-out "../shared/cond.rkt")
          (all-from-out "../shared/core.rkt")
          (all-from-out "../shared/numbers.rkt")
          (all-from-out "../shared/list.rkt")
 
-         (rename-out [glc-simply-typed-module-begin #%module-begin]
-                     [glc-simply-typed-def def]
+         (rename-out [glc-simply-typed-def def]
                      [glc-simply-typed-app #%app])
 
          :)
 
-(define-syntax (glc-simply-typed-module-begin stx)
-  (syntax-parse stx
-    [(_:id expr ...)
-     (begin
-       (get-requires #'(expr ...))
-
-       #'(#%module-begin
-          expr ...))]))
-
-(define-for-syntax (get-requires stx)
-  (syntax-parse stx
-    [((id:id arg) ~rest stx)
-     (begin
-       (if (equal? (syntax-e #'id) 'require)
-           (namespace-require (syntax-e #'arg))
-           (void))
-       (get-requires #'stx))]
-    [_ (void)]))
-
 (define-syntax (: stx)
   (syntax-parse stx
-    [(_ id:id type:id)
-     (with-syntax ([type-id (format-id stx "glc-type-~a" #'id)])
-       #'(define-for-syntax type-id '(type)))]
     [(_ id:id type:expr)
      (with-syntax ([type-id (format-id stx "glc-type-~a" #'id)])
        #'(define-for-syntax type-id 'type))]))
 
 (define-syntax (glc-simply-typed-app stx)
+  (define normalized (datum->syntax stx (normalize-app (cdr (syntax->datum stx)))))
+
   (when (equal? (syntax-local-context) 'module)
-    (displayln (nest-app (cdr (syntax->datum stx))))
-    (displayln stx))
+    (typecheck normalized (make-immutable-hash)))
 
-  (syntax-parse stx
-    [(_ x ...) (with-syntax ([app #'(glc-app x ...)])
-                 ;(displayln (nest-app #'(x ...)))
-                 ;(displayln #'(x ...))
-                 #'app)]))
-
-(define-for-syntax (nest-app dat [lst '()])
-  (if (and (list? dat) (<= 2 (length dat)))
-      (let*-values ([(abs?) (equal? (car dat) 'λ)]
-                    [(split) (if abs? 3 2)]
-                    [(left right) (split-at dat split)]
-                    [(empty-right?) (empty? right)]
-                    [(lleft) (car left)]
-                    [(rleft) (car (cdr left))])
-        (cond
-          [(and abs? empty-right?)
-           (list lleft rleft (nest-app (third left)))]
-          [empty-right? (cons (nest-app lleft) (list (nest-app rleft)))]
-          [else (nest-app (cons left right))]))
-
-      dat))
+  (syntax-parse normalized
+    ;; Not sure why app isn't automatically forceing here.
+    [(x y) #'(!! (#%app x y))]))
 
 (define-syntax (glc-simply-typed-def stx)
   (syntax-parse stx
     [(_ id:id expr:expr)
-     (with-syntax* ([type-id (format-id stx "glc-type-~a" #'id)])
-       ;(displayln (eval-syntax #'type-id))
-       (typecheck #'expr (make-immutable-hash (list (cons 'top (eval-syntax #'type-id)))))
-       #'(define id expr))]))
+     (with-syntax ([type-id (format-id stx "glc-type-~a" #'id)])
+       (let*-values ([(type) (eval-syntax #'type-id)]
+                     [(env) (make-immutable-hash)]
+                     [(ctx base) (set-context #'expr type env)]
+                     [(normalized) (datum->syntax base (normalize-app (syntax->datum base)))])
 
-(define-for-syntax (typecheck-lambda id expr env)
-  (define type-spec (hash-ref env 'top))
-  (define type-head (car type-spec))
-  (define type-tail (cdr type-spec))
+         (if (equal? (last type) (typecheck normalized ctx))
+             #'(define id expr)
+             (raise (format "~a does not have type ~a" (syntax-e #'id) type)))))]))
 
-  (when (> (length type-tail) 0)
-    (typecheck expr (hash-set* env [syntax-e id] type-head 'top type-tail))))
+(define-for-syntax (set-context stx type-id env)
+  (let ([type-head (car type-id)]
+        [type-tail (cdr type-id)])
+
+    (if (> (length type-tail) 0)
+        (syntax-parse stx
+          ; TODO add error case
+          [((~or (~literal λ) (~literal lambda)) (~or id:id (id:id)) expr)
+           (set-context #'expr type-tail (hash-set env [syntax-e #'id] type-head))])
+        (values env stx))))
 
 (define-for-syntax (typecheck stx env)
-  ;(displayln stx)
   (syntax-parse stx
-    [((~or (~literal λ) (~literal lambda)) (~or x:id (x:id)) expr)
-     (typecheck-lambda #'x #'expr env)]
-    [(_) (void)]
-    [_ (void)])
-  ;(displayln (syntax->list stx))
-  (void))
+    [((~or (~literal λ) (~literal lambda)) _ _)
+     (void)]
+    [x:id (with-syntax ([type-id (format-id stx "glc-type-~a" #'x)])
+            (hash-ref env (syntax-e #'x) (lambda () (eval-syntax #'type-id))))]
+    [(x y) (let* ([x-type (typecheck #'x env)]
+                  [y-type (typecheck #'y env)]
+                  [x1 (car x-type)]
+                  [xrest (cdr x-type)]
+                  [sx1 (format "~a" x1)])
 
-(define-for-syntax (get-type stx env)
-  (syntax-parse stx
-    [(x:id) (void)]
-    [(x:id y:expr)
-     (typecheck-lambda #'x #'expr env)]))
+             (let ([return-type
+                    (if (equal? y-type x1)
+                        xrest
+                        (if (equal? sx1 (string-downcase sx1))
+                            (map (lambda (var)
+                                   (if (equal? var x1)
+                                       y-type
+                                       var))
+                                 xrest)
+                            (raise (format "Type ~a does not match type ~a" y-type x-type))))])
+
+               (if (equal? (length return-type) 1)
+                   (car return-type)
+                   return-type)))]
+    [x (displayln #'x)]))
